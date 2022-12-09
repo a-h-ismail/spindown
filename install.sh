4ab4993596e59f8a0ddf295bdd8fe79c427ee839
Ahmad Ismail <ahmadismail2001@gmail.com>
2022-12-09 14:16:44 +0200

    Improved install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root."
   exit 1
@@ -10,6 +9,12 @@ fi
 g++ spindown.cpp -O2 -o spindownd
 systemctl daemon-reload
 mv spindownd /usr/bin
+echo 'Before proceeding, take note which disks should be tracked by the service.'
+echo 'For example, a disk with path /dev/sda is placed in the config as sda'
+echo 'Your disks list:'
+lsblk -d
+read -p 'Press enter to setup your config.'
+nano spindown.conf
 cp spindown.conf /etc
 cp spindown.service /usr/lib/systemd/system/
 #For SELinux users, just in case the binary is not labeled correctly
